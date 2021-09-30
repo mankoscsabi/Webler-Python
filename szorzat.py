972d2a65e018affda047cc4f634bd46c0d0b7c75
Csaba Miskolczi <mankoscsabi@outlook.com>
2021-09-30 15:04:10 +0200

    Create szorzat.py

---
+++ szorzat.py
@@ -0,0 +1,6 @@
+szam1= int(input("ird be az első szamot: \n"))
+szam2= int(input("ird be az masodik szamot: \n"))
+szam3= int(input("ird be az harmadik szamot: \n"))
+
+eredmeny=szam1*szam2*szam3
+print(eredmeny)
